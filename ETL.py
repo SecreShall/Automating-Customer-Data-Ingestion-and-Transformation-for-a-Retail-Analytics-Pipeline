1530389aeff0ef73f9ffa0c87cb663fd9ea24bdb
SecreShall <164751004+SecreShall@users.noreply.github.com>
2025-05-05 17:26:46 +0800

    added new file

--- ETL.py
+++ ETL.py
@@ -1,8 +1,4 @@
-#import libraries
 import pandas as pd
-from airflow.operators.python import PythonOperator
-from airflow import DAG
-from datetime import timedelta
 
 def extract():
     print('yameteee')
@@ -14,35 +10,3 @@ def transform():
 
 def load():
     pass
-
-
-default_args={
-     'owner': 'Cley',
-}
-
-dag = DAG(
-     dag_id='retail_etl',
-     default_args=default_args,
-     description='Retail Analytics Pipeline',
-     schedule=timedelta(days=1),
-)
-
-extract_task = PythonOperator(
-    task_id='extract',
-    python_callable=extract,
-    dag=dag,
-)
-
-transform_task = PythonOperator(
-    task_id='transform',
-    python_callable=transform,
-    dag=dag,
-)
-
-load_task = PythonOperator(
-    task_id='load',
-    python_callable=load,
-    dag=dag,
-)
-
-extract_task >> transform_task >> load_task
